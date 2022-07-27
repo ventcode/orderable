338925c0707d7afc7f0ea89ef82c6eabe9b6a895
kruczek <piotr.kruczek@ynd.co>
2022-07-27 19:41:53 +0200

    init

---
+++ lib/orderable/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Orderable
+  VERSION = "0.1.0"
+end
---
+++ spec/orderable_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Orderable do
+  it "has a version number" do
+    expect(Orderable::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
