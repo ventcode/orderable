b388146638c9c7948eafe1620cd82a8c907f1948
G_Zlamaniec <grzegorz.zlamaniec@ventcode.com>
2022-08-19 16:46:45 +0200

    SCOPES-TESTS: Deleted addons table

--- db/migrate/20220819081013_addons.rb
+++
@@ -1,9 +0,0 @@
-class Addons < ActiveRecord::Migration[6.1]
-  def change
-    create_table :addons do |t|
-      t.string :name, null: false
-      t.integer :position, null: false, default: 0
-      t.references :basic_model, foreign_key: true, index: false
-    end
-  end
-end
