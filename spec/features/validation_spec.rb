4988c092568732f2e992f7d118d7a3dc21c9da1a
Marek Mytkowski <marek.mytkowski.mm@gmail.com>
2022-08-19 12:52:00 +0200

    validate-tests: switch to shoulda allow_values after CR

--- spec/features/validation_spec.rb
+++ spec/features/validation_spec.rb
@@ -5,57 +5,58 @@ require 'support/models'
 
 RSpec.describe 'Validation', type: :model do
   context 'when validate option is set to true' do
-    before do
-      BasicModel.insert_all([
-        { name: 'a', position: 0 },
-        { name: 'b', position: 1 }
-      ])
-    end
-
-    subject { BasicModel.new(name: 'c', position: 2) }
+    subject { BasicModel.new(name: 'a', position: 0) }
 
     it { should validate_numericality_of(:position).only_integer.is_greater_than_or_equal_to(0) }
-    it 'validates that :position is less or equal to its current maximum possible value' do
-      expect(subject).to be_valid
+    
+    describe 'validates that :position is less or equal to its current maximum possible value' do
+      before do
+        BasicModel.insert_all([
+          { name: 'a', position: 0 },
+          { name: 'b', position: 1 }
+        ])
+      end
 
-      subject.position = 3
-      expect(subject).to be_invalid
+      context 'when model is persisted' do
+        subject { BasicModel.create(name: 'c', position: 2) }
 
-      subject.position = 1
-      expect(subject).to be_valid
+        it { should allow_values(*(0..2)).for(:position) }
+        it { should_not allow_values(-1, 3).for(:position)}
+      end
 
-      subject.save
+      context 'when model is not persisted' do
+        subject { BasicModel.new(name: 'c', position: 2) }
 
-      subject.position = 2
-      expect(subject).to be_valid
-
-      subject.position = 3
-      expect(subject).to be_invalid
-
-      subject.position = 1
-      expect(subject).to be_valid
+        it { should allow_values(*(0..2)).for(:position) }
+        it { should_not allow_values(-1, 3).for(:position)}
+      end
     end
   end
 
   context 'when validate option is set to false' do
-    before do
-      NoValidationModel.insert_all([
-        { name: 'a', position: 0 },
-        { name: 'b', position: 1 }
-      ])
-    end
-
     subject { NoValidationModel.new(name: 'c', position: 2) }
 
     it { should_not validate_numericality_of(:position).only_integer.is_greater_than_or_equal_to(0) }
-    it "doesn't validate that :position is less or equal to its current maximum possible value" do
-      subject.position = 3
-      expect(subject).to be_valid
+    
+    describe 'doesn\'t validate that :position is less or equal to its current maximum possible value' do
+      before do
+        NoValidationModel.insert_all([
+          { name: 'a', position: 0 },
+          { name: 'b', position: 1 }
+        ])
+      end
 
-      subject.save
+      context 'when model is persisted' do
+        subject { NoValidationModel.create(name: 'c', position: 2) }
 
-      subject.position = 3
-      expect(subject).to be_valid
+        it { should allow_values(*(-1..3)).for(:position) }
+      end
+
+      context 'when model is not persisted' do
+        subject { NoValidationModel.new(name: 'c', position: 2) }
+
+        it { should allow_values(*(-1..3)).for(:position) }
+      end
     end
   end
 end
