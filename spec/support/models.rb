125075b6d7773da683ebe8505242f31266224ccf
G_Zlamaniec <grzegorz.zlamaniec@ventcode.com>
2022-08-19 13:38:08 +0200

    SCOPE-TESTS: Added new model related to BasicModel

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class BasicModel < ActiveRecord::Base
+  has_many :addons
+
   orderable :position
+end
+
+class Addon < ActiveRecord::Base
+  belongs_to :BasicModel
+
+  orderable :position, scope: :basic_model_id
 end
