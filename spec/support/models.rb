e1ef20b2354fd60ffd96320a01663eece7667197
G_Zlamaniec <grzegorz.zlamaniec@ventcode.com>
2022-08-19 16:43:41 +0200

    SCOPES-TESTS: Added new models to tests

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 class BasicModel < ActiveRecord::Base
-  has_many :addons
-
   orderable :position
 end
 
-class Addon < ActiveRecord::Base
-  belongs_to :BasicModel
+class ScopesModel < ActiveRecord::Base
+end
 
-  orderable :position, scope: :basic_model_id
+class KindScopeModel < ScopesModel 
+  orderable :position, scope: :kind
+end
+
+class GroupScopeModel < ScopesModel 
+  orderable :position, scope: :group
 end
