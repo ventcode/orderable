0671902c16965276ebe50ddf09e5d896a0bb5c3f
G_Zlamaniec <grzegorz.zlamaniec@ventcode.com>
2022-08-23 13:02:59 +0200

    Added models and tests for reset function

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -7,19 +7,37 @@ end
 class ScopesModel < ActiveRecord::Base
 end
 
-class KindScopeModel < ScopesModel 
+class KindScopeModel < ScopesModel
   orderable :position, scope: :kind
 end
 
-class GroupScopeModel < ScopesModel 
+class GroupScopeModel < ScopesModel
   orderable :position, scope: :group
 end
 
 class SingleElementScope < BasicModel
-  attr_reader :executor
+  attr_accessor :executor
   include Orderable
 
-  before_create do
-    @executor = Executor.new(self, :position, 'scope')
+end
+
+class Executor < Orderable::Executor
+end
+
+class MultiDataBaseModel < ScopesModel
+  orderable :position, scope: :kind
+
+  def self.set_db_to_sqlite
+    ActiveRecord::Base.establish_connection(
+      adapter: 'sqlite3',
+      database: 'db/development.sqlite3',
+  )
+  end
+
+  def self.set_db_to_postgresql
+    ActiveRecord::Base.establish_connection(
+      adapter: 'postgresql',
+      database: 'orderable_development'
+  )
   end
 end
