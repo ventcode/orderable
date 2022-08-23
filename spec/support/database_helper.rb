85cc62eae10347c69fa1d4ad725c16f03942454a
G_Zlamaniec <grzegorz.zlamaniec@ventcode.com>
2022-08-23 15:11:57 +0200

    Changed for PostgreSQL

--- spec/support/database_helper.rb
+++ spec/support/database_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  database: 'db/development.sqlite3'
+  adapter: 'postgresql',
+  database: 'orderable_development'
 )
