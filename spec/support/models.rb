802f6fe37afdd68eefbfbbbf882f353552aa802b
Marek Mytkowski <101755133+mytkom@users.noreply.github.com>
2022-08-24 14:30:01 +0200

    Validate tests (#1)
    * validate-tests: rename basic_model_spec
    
    * validate-tests: add shoulda matchers gem
    
    * validate-tests: add NoValidationModel test model
    
    * validate-tests: add validation tests
    
    * validate-tests: switch to shoulda allow_values after CR
    
    * pull request action
    
    * dotfiles
    
    * ruby 3.0
    
    * add linux to lock
    
    * fixes
    
    * add rubocop correction
    
    * change type: :model rspec option to something more readable
    
    * rename description of validation tests
    
    Co-authored-by: kruczek <piotr.kruczek@ynd.co>
    Co-authored-by: Piotr Kruczek <78352648+mr-kruczek@users.noreply.github.com>

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -3,3 +3,9 @@
 class BasicModel < ActiveRecord::Base
   orderable :position
 end
+
+class NoValidationModel < ActiveRecord::Base
+  self.table_name = 'basic_models'
+
+  orderable :position, validate: false
+end
