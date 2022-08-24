a3ec447f8fb566770f84c88ab52a88070506421c
kruczek <piotr.kruczek@ynd.co>
2022-08-24 11:53:01 +0200

    use standalone migrations for test db

--- spec/support/database_helper.rb
+++ spec/support/database_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-ActiveRecord::Base.establish_connection(
-  adapter: 'postgresql',
-  database: 'orderable_development'
-)
+require 'tasks/standalone_migrations'
+
+config = StandaloneMigrations::Configurator.load_configurations
+db_config = config.fetch(ENV['RAILS_ENV'], 'development')
+
+ActiveRecord::Base.establish_connection(db_config)
