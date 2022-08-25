f58402a42707efce4ed3f19de63869f9c116d0ca
Marek Mytkowski <marek.mytkowski.mm@gmail.com>
2022-08-25 10:53:08 +0200

    default-push-last: add two new models for testing

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -9,3 +9,13 @@ class NoValidationModel < ActiveRecord::Base
 
   orderable :position, validate: false
 end
+
+class NoDefaultPushLastModel < ActiveRecord::Base
+  self.table_name = 'basic_models'
+
+  orderable :position, default_push_last: false
+end
+
+class NoDefaultModel < ActiveRecord::Base
+  orderable :position, default_push_last: false
+end
