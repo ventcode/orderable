f9416030c2da9284a50c22002e283408d27c4ad1
kruczek <piotr.kruczek@ynd.co>
2022-08-29 15:21:51 +0200

    CR

--- spec/features/scope_spec.rb
+++ spec/features/scope_spec.rb
@@ -9,77 +9,35 @@ RSpec.shared_examples 'does not affect outside of scope' do
   end
 end
 
-RSpec.describe 'asdasdasd' do
+RSpec.describe 'Configuration option :scope' do
   before do
     ModelWithOneScope.insert_all([
-      { name: 'a', position: 0, kind: 'alpha' },
-      { name: 'b', position: 1, kind: 'alpha' },
-      { name: 'c', position: 2, kind: 'alpha' }
-    ])
-
-    ModelWithManyScopes.insert_all([
-      { name: 'a', position: 0, kind: 'beta', group: 'a' },
-      { name: 'b', position: 1, kind: 'beta', group: 'a' },
-      { name: 'c', position: 2, kind: 'beta', group: 'a' }
+      { name: 'a', position: 0, kind: 'alpha', group: 'a' },
+      { name: 'b', position: 1, kind: 'alpha', group: 'a' },
+      { name: 'c', position: 2, kind: 'alpha', group: 'a' }
     ])
   end
 
-  let!(:out_of_single_scope) { ModelWithOneScope.create!(name: 'other', position: 0, kind: 'beta') }
-  let!(:out_of_many_scopes) { ModelWithManyScopes.create!(name: 'other', position: 0, kind: 'beta', group: 'b') }
+  let!(:out_of_scope) { ModelWithOneScope.create!(name: 'other', position: 0, kind: 'beta', group: 'b') }
 
-  context 'when creating a new record' do
-    let(:single_scope_action) { ModelWithOneScope.create(name: 'd', position: 0, kind: 'alpha') }
-    let(:many_scope_action) { ModelWithManyScopes.create(name: 'd', position: 0, kind: 'beta', group: 'a') }
+  [ModelWithManyScopes, ModelWithOneScope].each do |model|
+    describe model do
+      context 'when creating a new record' do
+        let(:action) { model.create(name: 'd', position: 0, kind: 'alpha', group: 'a') }
 
-    context 'for single scope' do
-      it_should_behave_like 'does not affect outside of scope' do
-        let(:action) { single_scope_action }
-        let(:out_of_scope) { out_of_single_scope }
+        include_examples 'does not affect outside of scope'
       end
-    end
 
-    context 'for many scopes' do
-      it_should_behave_like 'does not affect outside of scope' do
-        let(:action) { many_scope_action }
-        let(:out_of_scope) { out_of_many_scopes }
+      context 'when updating a record' do
+        let(:action) { model.where(kind: 'alpha', group: 'a').last.update(position: 0) }
+
+        include_examples 'does not affect outside of scope'
       end
-    end
-  end
 
-  context 'when updating a record' do
-    let(:single_scope_action) { ModelWithOneScope.where(group: nil).last.update(position: 0) }
-    let(:many_scope_action) { ModelWithManyScopes.where.not(group: nil).last.update(position: 0) }
+      context 'when destroying a record' do
+        let(:action) { model.where(kind: 'alpha', group: 'a').last.destroy }
 
-    context 'for single scope' do
-      it_should_behave_like 'does not affect outside of scope' do
-        let(:action) { single_scope_action }
-        let(:out_of_scope) { out_of_single_scope }
-      end
-    end
-
-    context 'for many scopes' do
-      it_should_behave_like 'does not affect outside of scope' do
-        let(:action) { many_scope_action }
-        let(:out_of_scope) { out_of_many_scopes }
-      end
-    end
-  end
-
-  context 'when destroying a record' do
-    let(:single_scope_action) { ModelWithOneScope.where(group: nil, kind: 'alpha').last.destroy }
-    let(:many_scope_action) { ModelWithManyScopes.where(kind: 'beta', group: 'a').last.destroy }
-
-    context 'for single scope' do
-      it_should_behave_like 'does not affect outside of scope' do
-        let(:action) { single_scope_action }
-        let(:out_of_scope) { out_of_single_scope }
-      end
-    end
-
-    context 'for many scopes' do
-      it_should_behave_like 'does not affect outside of scope' do
-        let(:action) { many_scope_action }
-        let(:out_of_scope) { out_of_many_scopes }
+        include_examples 'does not affect outside of scope'
       end
     end
   end
