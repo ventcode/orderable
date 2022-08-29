91650c2030b4cee2e46ed77d3f94ab9c318f91c8
G_Zlamaniec <grzegorz.zlamaniec@ventcode.com>
2022-08-29 14:18:38 +0200

    Fixed scope tests

--- spec/features/scope_spec.rb
+++ spec/features/scope_spec.rb
@@ -3,19 +3,9 @@
 require 'support/database_helper'
 require 'support/models'
 
-RSpec.shared_examples 'does not affect for single scope' do |_actions|
-  let(:actions) { instance_exec(&action) }
-
+RSpec.shared_examples 'does not affect outside of scope' do
   it 'does not affect records outside of scope' do
-    expect { single_scope_action }.not_to change(out_of_single_scope.reload, :position)
-  end
-end
-
-RSpec.shared_examples 'does not affect for many scopes' do |_actions|
-  let(:actions) { instance_exec(&action) }
-
-  it 'does not affect records outside of scope' do
-    expect { many_scope_action }.not_to change(out_of_many_scopes.reload, :position)
+    expect { action }.not_to change { out_of_scope.reload.position }
   end
 end
 
@@ -35,29 +25,50 @@ RSpec.describe 'asdasdasd' do
   end
 
   let!(:out_of_single_scope) { ModelWithOneScope.create!(name: 'other', position: 0, kind: 'beta') }
-  let!(:out_of_many_scopes) { ModelWithManyScopes.create!(name: 'other', position: 0, kind: 'alpha', group: 'a') }
+  let!(:out_of_many_scopes) { ModelWithManyScopes.create!(name: 'other', position: 0, kind: 'beta', group: 'b') }
 
   context 'when creating a new record' do
     let(:single_scope_action) { ModelWithOneScope.create(name: 'd', position: 0, kind: 'alpha') }
-    let(:many_scope_action) { ModelWithManyScopes.create(name: 'd', position: 0, kind: 'alpha', group: 'a') }
+    let(:many_scope_action) { ModelWithManyScopes.create(name: 'd', position: 0, kind: 'beta', group: 'a') }
 
-    it_should_behave_like 'does not affect for single scope', actions: -> { single_scope_action out_of_single_scope }
-    it_should_behave_like 'does not affect for many scopes', actions: -> { many_scope_action out_of_many_scopes }
+    it_should_behave_like 'does not affect outside of scope' do
+      let(:action) { single_scope_action }
+      let(:out_of_scope) { out_of_single_scope }
+    end
+
+    it_should_behave_like 'does not affect outside of scope' do
+      let(:action) { many_scope_action }
+      let(:out_of_scope) { out_of_many_scopes }
+    end
   end
 
   context 'when updating a record' do
     let(:single_scope_action) { ModelWithOneScope.where(group: nil).last.update(position: 0) }
     let(:many_scope_action) { ModelWithManyScopes.where.not(group: nil).last.update(position: 0) }
 
-    it_should_behave_like 'does not affect for single scope', actions: -> { single_scope_action out_of_single_scope }
-    it_should_behave_like 'does not affect for many scopes', actions: -> { many_scope_action out_of_many_scopes }
+    it_should_behave_like 'does not affect outside of scope' do
+      let(:action) { single_scope_action }
+      let(:out_of_scope) { out_of_single_scope }
+    end
+
+    it_should_behave_like 'does not affect outside of scope' do
+      let(:action) { many_scope_action }
+      let(:out_of_scope) { out_of_many_scopes }
+    end
   end
 
   context 'when destroying a record' do
-    let(:single_scope_action) { ModelWithOneScope.where(group: nil).last.destroy }
-    let(:many_scope_action) { ModelWithManyScopes.where.not(group: nil).last.destroy }
+    let(:single_scope_action) { ModelWithOneScope.where(group: nil, kind: 'alpha').last.destroy }
+    let(:many_scope_action) { ModelWithManyScopes.where(kind: 'beta', group: 'a').last.destroy }
 
-    it_should_behave_like 'does not affect for single scope', actions: -> { single_scope_action out_of_single_scope }
-    it_should_behave_like 'does not affect for many scopes', actions: -> { many_scope_action out_of_many_scopes }
+    it_should_behave_like 'does not affect outside of scope' do
+      let(:action) { single_scope_action }
+      let(:out_of_scope) { out_of_single_scope }
+    end
+
+    it_should_behave_like 'does not affect outside of scope' do
+      let(:action) { many_scope_action }
+      let(:out_of_scope) { out_of_many_scopes }
+    end
   end
 end
