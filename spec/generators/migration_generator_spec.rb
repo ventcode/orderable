44186f410446b97cf0458d96aee78f134a32cd09
Marek Mytkowski <marek.mytkowski.mm@gmail.com>
2022-09-02 16:55:02 +0200

    migration-generator: add migration generator tests

---
+++ spec/generators/migration_generator_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'generators/orderable/migration/migration_generator'
+
+RSpec.describe Orderable::Generators::MigrationGenerator, type: :generator do
+  destination File.expand_path('../../tmp', __dir__)
+
+  before do
+    prepare_destination
+    run_generator %w[BasicModel:PositionField]
+  end
+
+  describe 'the migration' do
+    subject { migration_file('db/migrate/add_unique_orderable_position_field_to_basic_model.rb') }
+
+    it { is_expected.to exist }
+    it { is_expected.to contain /execute/ }
+    it { is_expected.to contain /ALTER TABLE basic_models/ }
+    it { is_expected.to contain /ADD position_field INTEGER UNIQUE DEFERRABLE INITIALLY DEFERRED/ }
+    it { is_expected.to contain /DROP COLUMN position_field/ }
+  end
+end
