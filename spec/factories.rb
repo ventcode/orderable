6a81c4fe9923795139d35769162c4478232b009b
Mateusz Lata <mateusz.lata@ynd.co>
2023-05-26 16:30:50 +0200

    Update factories. Remove destroy_all & order_modification specs. Add callbacks spec.

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,18 +5,15 @@ require_relative "./support/models"
 FactoryBot.define do
   factory :basic_model, class: BasicModel do
     sequence(:name, "a")
-    sequence(:position, 0)
   end
 
   factory :model_with_one_scope, class: ModelWithOneScope do
     sequence(:name, "a")
-    sequence(:position, 0)
     kind { "alpha" }
   end
 
   factory :model_with_many_scopes, class: ModelWithManyScopes do
     sequence(:name, "a")
-    sequence(:position, 0)
     group { "a" }
     kind { "alpha" }
 
@@ -28,11 +25,9 @@ FactoryBot.define do
 
   factory :no_validation_model, class: NoValidationModel do
     sequence(:name, "a")
-    sequence(:position, 0)
   end
 
   factory :no_default_push_front_model, class: NoDefaultPushLastModel do
     sequence(:name, "a")
-    sequence(:position, 0)
   end
 end
